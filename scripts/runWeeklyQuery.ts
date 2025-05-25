66aaa017881218d372c6ef490678036e9db26239
fuzhenkai666 <fzklouis@qq.com>
2025-05-25 13:55:27 -0700

    瞎改的

--- scripts/runWeeklyQuery.ts
+++ scripts/runWeeklyQuery.ts
@@ -13,4 +13,4 @@ async function main() {
 
 main();
 //你需要在你的 Vercel 项目中配置 Cron Jobs，以确保脚本按照你的需求定期运行。你可以在 Vercel 的 Cron Jobs 配置界面中设置 Cron 表达式，以指定脚本的运行时间和频率。
-//本地运行测试node /Users/fuzhenkai/Desktop/meeting_record/scripts/runWeeklyQuery.ts
+//本地运行测试node /Users/fuzhenkai/Desktop/week/scripts/runWeeklyQuery.ts
